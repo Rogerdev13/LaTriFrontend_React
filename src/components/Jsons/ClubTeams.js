9e9c7b8df0e4e3b4b18a9356e67d93e82dab4d72
= <=>
2024-02-15 15:44:11 -0500

    Fixing the images urls

--- src/components/Jsons/ClubTeams.js
+++ src/components/Jsons/ClubTeams.js
@@ -4,21 +4,21 @@ export const ClubTeams = [
     {
         "id":1 ,
         "name" : "Ecuador" ,
-        "url" : "/src/img/groupe.jpeg"
+        "url" : "https://ep01.epimg.net/estaticos/arc/2022/11/mundial_qatar/genericos/images/media/equipos/ecu.jpg"
     },
     {
         "id":2 ,
         "name" : "Netherlands" ,
-        "url" : "/src/img/nether.jpg"
+        "url" : "https://assets.khelnow.com/news/uploads/2022/11/202206211405-main.cropped_1655795163.jpg"
     },
     {
         "id":3 ,
         "name" : "Senegal" ,
-        "url" : "/src/img/senegal.jpg"
+        "url" : "https://www.gulf-times.com/uploads/imported_images/Upload/Slider/11202212143529818319108.jpg"
     },
     {
         "id":4 ,
         "name" : "Qatar" ,
-        "url" : "/src/img/qatar.jpeg"
+        "url" : "https://studiofutbol.com.ec/wp-content/uploads/2022/11/qatar-2022-el-antes-y-despues-de-su-seleccion-tras-anunciarse-como-sede-del-mundial.jpg"
     }
 ]
--- src/components/Jsons/PartipionsWorldCup.js
+++ src/components/Jsons/PartipionsWorldCup.js
@@ -2,25 +2,25 @@ export const PartipionsWorldCup = [
     {
         "id": 1 ,
         "year": "2002",
-        "url":"/src/img/2002.jpeg",
+        "url":"https://media.primicias.ec/2020/06/30085514/ecuadormundial.jpg",
         "country": "Corea-Japon"
     },
     {
         "id":2 ,
         "year": "2006",
-        "url": "/src/img/2006.jpeg" ,
+        "url": "https://areachica.movix.com/wp-content/uploads/2020/03/ecuador.jpg" ,
         "country": 'Alemania'
     },
     {
         "id":3 ,
         "year": "2014",
-        "url": "src/img/2014.jpg" ,
+        "url": "https://www.teradeportes.com/sites/default/files/styles/flexslider_boxed_/public/images/titulares/2014/2014-06-09/lun.jpg?itok=BbOmHmml" ,
         "country": 'Brazil'
     },
     {
         "id":4 ,
         "year": "2022",
-        "url": "/src/img/2022.jpeg" ,
+        "url": "https://cnnespanol.cnn.com/wp-content/uploads/2022/11/GettyImages-1244929838-e1669102414683.jpg?quality=100&strip=info" ,
         "country": 'Qatar'
     }
 ]
